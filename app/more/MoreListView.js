a4da8cc0f1fe2488f6a414541a7002a758af37d9
Myron Hicks <hicks.myronj@gmail.com>
2018-01-23 16:21:33 -0600

    Membership Update form

--- app/more/MoreListView.js
+++ app/more/MoreListView.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, Linking } from 'react-native';
 import { Container, Header, Title, Body, Left, Right, Text, Content } from 'native-base';
 import { List, ListItem } from 'react-native-elements';
 
 export default class MoreListView extends Component {
+
+    openUrl = (url) => {
+        Linking.openUrl(url);
+    };
+
+    routeTo = ({item}) => {
+        if(item.route) {
+            this.props.navigation.navigate(item.route)
+        }else{
+            Linking.canOpenURL(item.url)
+                .then((supported) => {
+                    if (supported) {
+                        return Linking.openURL(item.url)
+                            .catch(() => null);
+                    }
+            });
+        }
+    };
+
     render() {
         const routes = [
             {id: 1, title: "About", route: 'About', icon: 'info-outline'},
-            {id: 3, title: "What We Believe", route: 'Believe', icon: 'info'},
+            {id: 3, title: "What We Believe", route: 'Believe', icon: 'info-outline'},
             {id: 4, title: "Our History", route: 'History', icon: 'highlight'},
             {id: 5, title: "Give", route: 'Give', icon: 'attach-money'},
-            // {id: 6, title: "Share", route: 'Share', icon: 'share'},
+            {id: 6, title: "Update Membership Info", url: 'https://docs.google.com/forms/d/e/1FAIpQLSdMDkN5ETQRDEQ0xmdkSqbEBKmrYOiVPks6LZ8kA3S3yxSj2A/viewform', icon: 'info'},
             {id: 7, title: "Leave Feedback", route: 'Feedback', icon: 'thumb-up'},
         ];
+
         return(
             <Container>
                 <Content style={{backgroundColor: '#f6f8fa'}}>
@@ -25,7 +45,7 @@ export default class MoreListView extends Component {
                                 key={item.id}
                                 title={item.title}
                                 leftIcon={{name: item.icon}}
-                                onPress={() => this.props.navigation.navigate(item.route)}
+                                onPress={_ => this.routeTo({item})}
                             />
                         )}
                     />
